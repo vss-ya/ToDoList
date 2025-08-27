a58664e6df4e6b5c1b9282b69938a41a67144451
vs <vss.direct@gmail.com>
2025-08-27 20:10:33 +0700

    XCode proj init

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by vs on 27.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
